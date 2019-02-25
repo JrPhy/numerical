07962d82cd00768523ded127fa7d1c795811b97a
JrPhy <j0958322080@gmail.com>
2019-02-25 14:17:59 +0800

    Update simpson.cpp

--- Integration/simpson.cpp
+++ Integration/simpson.cpp
@@ -1,43 +1,62 @@
 #include <stdio.h> 
-#include <math.h>
 #include <stdlib.h>
-int main(){
-	int i, a = 0, b = 1;
-	double g[n+1], h, y1 = 0, y2 = 0, sum = 0;
-	h = (double)(b-a)/n;
-	for(i=0; i<n+1; i++) g[i] = exp(i*h); //test function
-	// simpson's 1/3 method
-	if(n%2 == 0)
+double f(double x)
+{
+	return (x*x*x);
+}
+
+double simps(double y[], double x[], int n)
+{
+	int i;
+	double h = x[1] - x[0], y1 = 0, y2 = 0, sum = 0;
+	if((n-1)%2 == 0)
 	{
-		for(i=1;i<n;i++)
+		for(i=1;i<n-1;i++)
 		{
-			if(i%2==0) y1 += g[i];
-        		else y2 += g[i];
+			if(i%2==0) y1 += y[i];
+        	else y2 += y[i];
 		}
-		sum = h*(g[0]+2*y1+4*y2+g[n])/3;
+		sum = h*(y[0]+2*y1+4*y2+y[n-1])/3;
 	}
-	// simpson's 1/3 method
-	// simpson's 3/8 method
-	else if(n%3 == 0)
+	else if((n-1)%3 == 0)
 	{
-		for(i=1;i<n;i++)
+		for(i=1;i<n-1;i++)
 		{
-			if(i%3==0) 	y1 += g[i];
-        		else y2 += g[i];
+			if(i%3==0) 	y1 += y[i];
+        	else y2 += y[i];
 		}
-		sum = 3*h*(g[0]+2*y1+3*y2+g[n])/8;
+		sum = 3*h*(y[0]+2*y1+3*y2+y[n-1])/8;
 	}
-	// simpson's 3/8 method
-	// combine simpson's 1/3 and 3/8 method
 	else
 	{
-		for(i=1;i<n-3;i++)
+		for(i=1;i<n-4;i++)
 		{
-			if(i%2==0) y1 += g[i];
-        		else y2 += g[i];
+			if(i%2==0) y1 += y[i];
+        	else y2 += y[i];
 		}
-		sum = h*(g[0]+2*y1+4*y2+g[n-3])/3 + 3*h*(g[n-3]+3*g[n-2]+3*g[n-1]+g[n])/8;
+		sum = h*(y[0]+2*y1+4*y2+y[n-4])/3 + 3*h*(y[n-4]+3*y[n-3]+3*y[n-2]+y[n-1])/8;
 	}
-	// combine simpson's 1/3 and 3/8 method
-return 0;
+	return sum;
+}
+
+int main(){
+	for(int n=2; n<20; n++)
+	{
+		int i;
+		double *x, *y, h, a = 1.0, b = 4.0, z;
+		y = (double*)malloc((n+1)*sizeof(double));
+		x = (double*)malloc((n+1)*sizeof(double));
+		h = (double)(b-a)/n;
+		for(i=0;i<n+1;i++)
+		{
+			x[i] = a + i*h;
+			z = x[i];
+			y[i] = f(z);
+			printf("%f\n",y[i]);
+		}
+		printf("sum = %f\n",simps(y,x,n+1));
+		free(y);
+		free(x);
+	}
+	return 0;
 }
