15b0edaea83471b018fe6015cf1f8ce4c282abf0
JrPhy <j0958322080@gmail.com>
2019-11-04 18:03:34 +0800

    Update Fibonacci number.c

--- Fibonacci number/Fibonacci number.c
+++ Fibonacci number/Fibonacci number.c
@@ -1,7 +1,26 @@
+#include<stdio.h> 
+
 int Fibonacci(int n)
 {
-	int i, F[n+1];
-	F[0] = 1;F[1] = 1;
-	for(i=2;i<n+1;i++) F[i] = F[i-1] + F[i-2];
-	return F[n];
+	if (n == 0) return 0;
+	else if( n == 2 || n == 1) return 1;
+	else
+	{
+		int fzero, fone, ftwo, i;
+		fzero = 1;
+		fone = 1;
+		for(i=2;i<n;i++) 
+		{
+			ftwo = fone + fzero;
+			fzero = fone;
+			fone = ftwo;
+		}
+		return ftwo;
+	}
+}
+
+int main()
+{
+	int i;
+	for(i=0; i<15; i++) printf("%d	%d\n",i,Fibonacci(i));
 }
